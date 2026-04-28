ba2a23cfe23ea3ef497bb427eb7fd8a116066c22
Ekram-Miftah <ekrammiftah013@gmail.com>
2026-04-28 19:40:55 +0300

    pagination check

--- src/index.ts
+++ src/index.ts
@@ -7,15 +7,39 @@ async function main() {
   await mongoose.connect(uri);
   console.log("Connected Successfully to MonoDb");
 
-  const product = new Product({
-    name: "Mechanical Keyboard",
-    category: "Electronics",
-    price: 149.99,
-    inStock: true,
-    tags: ["Keyboard", "peripheral", "gaming"],
-  });
-  const result = await product.save();
-  console.log("Saved :", result);
+  //   const all = await Product.find();
+  //   console.log("All Products:", all);
+  //   const names = await Product.find().select("name price -_id");
+  //   console.log("Name Products:", names);
+  //   const sorted = await Product.find().select("name price -_id").sort("price");
+  //   console.log(
+  //     "Sorted Products:",
+  //     sorted.map((p) => p.name)
+  //   );
+
+  //   const cheap = await Product.find({ price: { $lt: 50 } });
+  //   console.log("checheapap Products:", cheap);
+
+  //   const midRange = await Product.find({ price: { $gte: 30, $lt: 150 } }).select(
+  // //     "name price -_id"
+  // //   );
+  // //   console.log("30-150 Products:", midRange);
+
+  //   const Filtered = await Product.find({
+  //     category: { $in: ["Electronics ", "Accessories"] },
+  //   }).select("name price -_id");
+  //   console.log("filtered elec and acess  Products:", Filtered);
+
+  const pageSize = 3;
+  const page = 2;
+  const paginated = await Product.find()
+    .sort("name")
+    .skip((page - 1) * page)
+    .limit(pageSize);
+  console.log(
+    `Page ${page}:`,
+    paginated.map((p) => p.name)
+  );
 }
 main().catch((err) => {
   console.log("Connected Failed  to MonoDb:", err);
